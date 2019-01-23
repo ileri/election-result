727bb0ffe13a3d765426313885b6a03972d1e6eb
ileri <celil.ileri@bil.omu.edu.tr>
2019-01-24 00:21:44 +0300

    conver homepage to slim, show inactive elections on homepage

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,11 +1,12 @@
 class Admin::HomeController < AdminController
-  before_action :active_elections
+  before_action :get_active_inactive_elections
   def index
     @boxes = Box.all
   end
 
   private
-  def active_elections
+  def get_active_inactive_elections
     @active_elections = Election.where(active: true)
+    @inactive_elections = Election.where(active: false)
   end
 end
